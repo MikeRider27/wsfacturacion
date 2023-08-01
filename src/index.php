7c9d8e5a61f06ce679b0cf363fec1cf799843cd2
Miguel Villalba <mike.mavc27@gmail.com>
2023-08-01 18:23:35 -0400

    fixed document type validation function

--- src/index.php
+++ src/index.php
@@ -1,36 +1,17 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-// Obtener la ruta solicitada sin los parámetros de consulta
-$request_uri = $_SERVER['REQUEST_URI'];
-$request_path = parse_url($request_uri, PHP_URL_PATH);
-
-// Rutas disponibles y sus controladores asociados
-$routes = [
-    //'/' => 'IndexController@index', 
-    '/' => 'XmlController@handleRequest',
-    // Agrega aquí más rutas si es necesario
-];
-
-// Buscar la ruta solicitada en la lista de rutas disponibles
-$matched = false;
-foreach ($routes as $route => $handler) {
-    if ($request_path === $route) {
-        list($controller, $method) = explode('@', $handler);
-        require_once 'controllers/' . $controller . '.php';
-        $controllerInstance = new $controller();
-        $controllerInstance->{$method}();
-        $matched = true;
-        break;
-    }
+// Enrutador básico
+function routeHandler($route)
+{
+    if ($route === '/') {
+        echo "Bienvenido a la página de inicio.";
+    } elseif ($route === '/generate-xml') {
+        echo "Esta es la página de información acerca de nosotros.";
+    } 
 }
 
-// Si no se encuentra una ruta coincidente, muestra un mensaje de error o redirecciona a una página 404
-if (!$matched) {
-    header("HTTP/1.0 404 Not Found");
-    echo "Error 404: Página no encontrada";
-    // También puedes redireccionar a una página de error personalizada.
-}
+// Obtener la ruta actual de la solicitud (usando la variable $_SERVER['REQUEST_URI'])
+$current_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// Manejar la ruta
+routeHandler($current_uri);
+?>
--- src/services/addDefaultValues.php
+++ src/services/addDefaultValues.php
@@ -1,17 +1,66 @@
 <?php
+
 require 'ConstantsService.php';
 
 class addDefaultValues {
 
     public function DefaultValues(&$data) {
-
         $tipoDocumento = $data[0]['tipoDocumento'];
 
-        var_dump($tipoDocumento);
+        // Suponiendo que la clase ConstantService tiene un método getTiposDocumentos() que devuelve el array tiposDocumentos
         $constanteServiceInstance = new ConstantService();
         $tiposDocumentos = $constanteServiceInstance->getTiposDocumentos();
 
+        $success = false;
+        foreach ($tiposDocumentos as $um) {
+            if ($um['codigo'] === $tipoDocumento) {
+                $data[0]['tipoDocumentoDescripcion'] = $um['descripcion'];
+                $success = true;
+                break;
+            }
+        }
 
-       
+        if (!$success) {
+            // Generando el mensaje de error en formato JSON con los valores válidos del array $tiposDocumentos
+            $valoresValidos = array_map(function ($um) {
+                return $um['codigo'];
+            }, $tiposDocumentos);
+
+            $errorMensaje = [
+                'success' => false,
+                'error' => "Error en el campo tipoDocumento. Los valores válidos son: " . implode(', ', $valoresValidos) . "."
+            ];
+
+           echo json_encode($errorMensaje);
+           exit;
+        }
+
+        // El resto del código sigue siendo el mismo...
+        if (!isset($data[0]['tipoEmision'])) {
+            $data[0]['tipoEmision'] = 1;
+        }
+
+        if (!isset($data[0]['tipoTransaccion'])) {
+            $data[0]['tipoTransaccion'] = 1;
+        }
+
+        if (!isset($data[0]['moneda'])) {
+            $data[0]['moneda'] = 'PYG';
+        }
+
+        if ($data[0]['moneda'] !== 'PYG') {
+            if (!isset($data[0]['condicionTipoCambio'])) {
+                $data[0]['condicionTipoCambio'] = 1; // Por el Global
+            }
+        }
+
+        // Valores por defecto para los items
+        if (isset($data[0]['items']) && is_array($data[0]['items']) && count($data[0]['items']) > 0) {
+            foreach ($data[0]['items'] as &$item) {
+                if (!isset($item['unidadMedida'])) {
+                    $item['unidadMedida'] = 77;
+                }
+            }
+        }
     }
 }
